62e9d3b568a01e6c615d1639989501e1d04a67b5
Cecilia Clohisy <ceciliaclohisy@Cecilias-MacBook-Pro.local>
2018-03-18 19:22:05 -0500

    gifs functioning

--- app.js
+++ app.js
@@ -3,107 +3,137 @@ console.log("i am here")
 
 $(document).ready(function () {
 
-//establish nessicary variables
-//userInput
-//create topics array
-//this will be the tile of the buttons displayed
-var topics = ["mother of dragons","cersei", "jon snow" , "GOT dragons", "tyrion", "jamie lannister", "joffery", "white walkers", "bran", "direwolf", "winter is coming", "arya", "ramsay bolton"]
-console.log(topics)
+    //establish nessicary variables
+    //userInput?
+    
+    //create topics array
+    //this will be the tile of the buttons displayed
+    var topics = ["mother of dragons", "cersei", "jon snow", "GOT dragons", "tyrion", "jamie lannister", "joffery", "white walkers", "direwolf", "winter is coming", "arya", "ramsay bolton"]
+    console.log(topics)
 
-//create function to display buttons
-    //function displayButtons
-//loop through topics array
-// for (i=0; i<topics.lenghth; i++) 
-    //create button for each item in array
-        //var topicButton 
+
+    //create function to display buttons
+    function displayButtons() {
+        //empty div so only one set of buttons is displayed as new ones are added
+        $("#buttonDiv").empty()
+        //loop through topics array
+        for (i = 0; i < topics.length; i++) {
+            //create button for each item in array
+            var topicButton = $("<button>")
+            //add classes for formatting
+            topicButton.attr("class", " topicBtn m-2 btn btn-light")
+            //display text of ith item in button
+            topicButton.text(topics[i])
+            
             //assign data-topic attribute = [i] ? ... string value from array?
-            //button should be titled with the text from the array
-    //display (append) new button in buttons div
+            topicButton.attr("data-topic", topics[i])
+            //display (append) new button in buttons div
+            $("#buttonDiv").append(topicButton)
+        }
+    }//end of displayButtons funciton 
 
-    //eventually... add new put from user input 
-        //add userInput to topics array
-        //create new button with title of userInput
-
-//on click of topic button...
-    //grab data from giphy API
-        //set variable (var character?) to  get data-topic from button 
-        //set query URL variable 
-            //var queryURL = "......" + character + ".....API key"
-        //run ajax to get json data
-            //url = queryURL
-            //method = "GET"
-        //then run function to log response - log object to find what you are looking for 
-        //.then(function(response){}
-            //console.log(response)
-        // make a variable named results to store response.data
-            // var results = response.data;
-            //console.log(response.data);
-        //grab data and log what we need... 
-            //rating
-            //static image url...fixed height so display evenly?  
-            //animated image url... fixed height so display evenly? 
-        //once this is working... continue below :) 
-
-        //run loop to display search data for info we want... static image, rating, and animated gif? 
-        //only want 10 images to display
-        //for (var i = 0; i < 11; i++)
-            // Make a div to hold image tag and rating  and store in variable
-                //var gifDiv= $("<div>");
-            // Make a paragraph to hold rating and store in variable
-                //var gifRating = $("<p>");
-            // Set text of the paragraph to the rating of the image in results[i].
-                //gifRating.text(results[i].rating);     
-            //create image tag to hold static image
-                //var staticImage = $("<img>");
-                // Set the image's src to results[i]'s static image
-                    // staticImage.attr("src", results[i]........;
-                //set data-state attribute to still 
-                    //staticImage.attr("data-state", "still")
-
-            //create image tag to hold animated gif
-                //var animatedImage = $("<img>");
-                // Set the image's src to results[i]'s animated image
-                    // animatedImage.attr("src", results[i]........;
-                //set data-state attribute to animated 
-                    //animatedImage.attr("data-state", "animated")
-
-            // Append image rating variable to the gifDiv variable.
-                //gifDiv.append(gifRating);
-            // Append static image to the gifDiv variable.
-                //gifDiv.append(staticImage);
-            // append animated image to gifDiv
-                 //gifDiv.append(animatedImage);
-                 //hide animated image? 
-
-            // Prepend the animalDiv to html id of gifs-appear-here.
-                //$("#gifsHere").prepend(gifDiv);
-    //close on button click function 
-
-//on click of image
-    //make a variable named state and then store the image's data-state into it.
-      //var state = $(this).attr("data-state")   
-      //console.log(state)
-    //IF state === still 
-        //hide static image
-        //display animated image
-        //update data-state to animated
-    //ELSE If state == animated 
-        //hide animated image
-        //display static image
-        //update data-state to still
-
-//create function to display new buttons
-  // when submit button clicked..
-    //$("#submitButton").on("click", function(event) {
-                //event.preventDefault() - not sure what this is doing ... 
+    //calls display buttons function
+    displayButtons()
+    
+    //create function to display new buttons
+    // when submit button clicked..
+    $("#submitButton").on("click", function (event) {
+        // - not sure what this is doing ... stopping form from actually submitting?
+        event.preventDefault()
         //grab  userInput from textbox and set to variable
-            //var newTopic = $("#userInput").val()
+        var newTopic = $("#userInput").val()   
         //push newTopic into existing topics array
-            //topics.push(newTopic)
+        topics.push(newTopic)
+        console.log(topics)
+        //run display buttons function 
+        displayButtons()
 
-  //NOT SURE WHAT THIS IS DOING FROM CLASS EXAMPLE... MIGHT NOT BE NEEDED...
-    // Adding click event listeners to all elements with a class of "movie" - 
-    //$(document).on("click", ".movie", displayMovieInfo);
+    })//closes new button function
 
+
+    //on click of topic button...
+    $(".topicBtn").on("click", function () {
+        //empty div so only chosen gifs pop up
+        $("#gifsHere").empty()
+        //set variable (var character?) to  get data-topic from button 
+        var character = $(this).attr("data-topic");
+        //set query URL variable 
+        //only want 10 images to display.. add limit qualifier to url
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=8GCJbgGlR5BgmVfAFFIRJUhmgJOp4ou0&limit=10";
+
+        //run ajax to get json data from giphy API
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                //run loop to display search data for info we want... static image, rating, and animated gif? 
+                // make a variable named results to store response.data
+                var results = response.data;
+                console.log(results);
+                //grab data for each item in resutls array and set variables... display in #gifsHere
+                for (var i = 0; i < results.length; i++) {
+                    //create div for each gif w/rating
+                    var contentDiv = $("<div>");
+                    // Make a paragraph to hold rating and store in variable
+                    var gifRating = $("<h3>");
+                    //set class for formatting
+                    gifRating.attr("class", "text-light ")
+                    // Set text of the paragraph to the rating of the image in results[i].
+                    gifRating.text("Rating : " + results[i].rating);
+                    // Append image rating variable to the contentDiv variable.
+                    contentDiv.append(gifRating);
+
+                    //create image tag to hold gif
+                    var gifImg = $("<img>");
+                    // append gifImg to contentdiv
+                    contentDiv.append(gifImg);
+                    //set clases for formatting
+                    gifImg.attr("class", "img-responsive")
+
+                    // store animated source in variable 
+                    var animatedSource = results[i].images.fixed_height.url
+                    gifImg.attr("dataAnimated", animatedSource)
+                    // Store still source in variable
+                    var stillSource = results[i].images.fixed_height_still.url
+                    gifImg.attr("dataStill", stillSource)
+                    //set data state attribute to still 
+                    gifImg.attr("dataState", "still")
+
+                    //set img src to dataStill
+                    gifImg.attr("src", stillSource)
+                    // Append static image to the content div variable.
+                    contentDiv.append(gifImg);
+
+
+                    // Prepend the contentDiv to html id of gifsHere
+                    $("#gifsHere").prepend(contentDiv);
+
+                }
+
+            })
+    })//close on button click function 
+
+    //on click of image
+    $("#gifsHere").on("click", "img", function () {
+        // //make a variable named state and then store the image's data-state into it.
+        var state = $(this).attr("dataState")
+        //IF state === still
+        if (state == "still") {
+            //update the src attribute img to animateSource value,
+            $(this).attr("src", $(this).attr("dataAnimated"));
+            // update data state to animated
+            $(this).attr("dataState", "animate");
+        }
+        //else if
+        else if (state == "animate") {
+            // update the src attribute img to animateSource value,
+            $(this).attr("src", $(this).attr("dataStill"));
+            //update data state to stil 
+            $(this).attr("dataState", "still");
+        }
+    
+    })//closes static img click function
 
 })// closes document ready function 
